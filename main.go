0bb0a412e812baa664bb06c6473e4295fe85839c
alejoboga-io <alejo.boga@pendo.io>
2024-09-24 16:49:27 -0300

    hello world gin

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") // listen and serve on localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Hello World",
+	})
 }
