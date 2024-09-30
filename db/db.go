3866ac9d0811db5cdced991a4f2fbedeb43eb70f
alejoboga-io <alejo.boga@pendo.io>
2024-09-30 16:06:55 -0300

    create users table

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -23,6 +24,21 @@ func InitDB() {
 }
 
 func createTables() {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+		)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not create users table.")
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,13 +46,15 @@ func createTables() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 		)
 	`
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
+		fmt.Println(err)
 		panic("Could not create events table.")
 	}
 }
