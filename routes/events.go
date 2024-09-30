b03e610fff25bafbbec5d47525d952c3d66a4d30
alejoboga-io <alejo.boga@pendo.io>
2024-09-30 15:45:26 -0300

    delete events

--- routes/events.go
+++ routes/events.go
@@ -109,3 +109,36 @@ func updateEvent(context *gin.Context) {
 		"message": "Event updated successfully.",
 	})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid event id.",
+		})
+		return
+	}
+
+	eventToDelete, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"error": "Could not get event.",
+		})
+		return
+	}
+
+	err = eventToDelete.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Could not delete event.",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event deleted successfully.",
+	})
+}
