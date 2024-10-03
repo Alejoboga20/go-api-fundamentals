b9b8f53068b1640a5dca29dc6279b5170be5c2f5
alejoboga-io <alejo.boga@pendo.io>
2024-10-03 09:24:20 -0300

    test registration

--- routes/register.go
+++ routes/register.go
@@ -35,11 +35,11 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
-	userId := context.GetInt64("userId")
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	// userId := context.GetInt64("userId")
+	// eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event id."})
-		return
-	}
+	// if err != nil {
+	// 	context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event id."})
+	// 	return
+	// }
 }
